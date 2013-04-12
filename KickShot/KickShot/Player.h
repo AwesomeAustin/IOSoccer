af372ef3586518448721abb4263f09ff89c08923
Austin <austin@Austins-Mac.local>
2013-04-11 21:10:27 -0700

    more work on backworkings plus first try images for front screen

---
+++ KickShot/KickShot/Player.h
@@ -0,0 +1,13 @@
+//
+//  Player.h
+//  KickShot
+//
+//  Created by Austin on 4/9/13.
+//  Copyright (c) 2013 Austin Enfield. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Player : NSObject
+
+@end
---
+++ KickShot/KickShot/Player.m
@@ -0,0 +1,13 @@
+//
+//  Player.m
+//  KickShot
+//
+//  Created by Austin on 4/9/13.
+//  Copyright (c) 2013 Austin Enfield. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+@end
