3a8ce261433d5663319197b02a340ab1289a512d
Austin <austin@Austins-Mac.local>
2013-04-16 21:11:19 -0700

    more update on cards

--- KickShot/Card.h
+++ KickShot/Card.h
@@ -2,12 +2,40 @@
 //  Card.h
 //  KickShot
 //
-//  Created by Austin on 4/16/13.
+//  Created by Austin on 4/4/13.
 //  Copyright (c) 2013 Austin Enfield. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
+
+typedef enum {
+    header, endCName//fill in the rest of the cards and off cards
+} CName;
+
+typedef enum {
+    home, away, reff, endCType
+} CType;//what team they belong to
+
+typedef enum {
+    offense, defense, official, endPlayType
+} PlayType;//how you play them
+
+
+
 @interface Card : NSObject
+{
+    
+    CType team;
+    CName value;
+    PlayType type;
+    
+}
+
+@property (nonatomic) CName value;
+@property (nonatomic) CType team;
+@property (nonatomic) PlayType type;
+
+- (id) initWithValue:(CName) aValue type:(PlayType) aPlay;
 
 @end
--- KickShot/Card.m
+++ KickShot/Card.m
@@ -2,12 +2,25 @@
 //  Card.m
 //  KickShot
 //
-//  Created by Austin on 4/16/13.
+//  Created by Austin on 4/4/13.
 //  Copyright (c) 2013 Austin Enfield. All rights reserved.
 //
 
 #import "Card.h"
 
+
 @implementation Card
+
+@synthesize value, type;
+@synthesize team;
+
+
+- (id) initWithValue:(CName) aValue type:(PlayType) aPlay{
+	if(self = [super init]) {
+		self.value = aValue;
+        self.type = aPlay;
+	}
+	return self;
+}
 
 @end
