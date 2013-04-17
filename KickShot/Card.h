48fe4e5bb944e1b72839f850e59174c6ece2f135
Austin <austin@Austins-Mac.local>
2013-04-16 21:08:33 -0700

    fixed discrepency getting rid of errors in card files

---
+++ KickShot/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  KickShot
+//
+//  Created by Austin on 4/16/13.
+//  Copyright (c) 2013 Austin Enfield. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ KickShot/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  KickShot
+//
+//  Created by Austin on 4/16/13.
+//  Copyright (c) 2013 Austin Enfield. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
